48366a7dd07f9d01ae5108066d07d4ec900c45b4
Elias Martins Guerra Prado <34711222+Eliasmgprado@users.noreply.github.com>
2018-03-19 16:19:56 -0300

    update

--- config.py
+++ config.py
@@ -16,3 +16,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['eliasprado.flask@gmail.com']
+
+    #POSTS CONFIG
+    POSTS_PER_PAGE = 3
